2fe1db707f3c4e59c4dd05576c43a6e9e8665ae6
RutamBhagat <rutambhagat@gmail.com>
2024-09-21 21:06:54 +0530

    feat(gallery): change image tag to image component

--- src/components/images.tsx
+++ src/components/images.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { getMyImages } from "~/server/queries/images";
@@ -10,7 +11,13 @@ async function Images() {
       {images.map((image) => (
         <div key={image.key} className="w-48">
           <Link href={`/image/${image.key}`}>
-            <img src={image.url} alt={image.name} />
+            <Image
+              src={image.url}
+              alt={image.name}
+              width={Math.round(Math.sqrt(image.size))}
+              height={Math.round(Math.sqrt(image.size))}
+              style={{ objectFit: "contain" }}
+            />
           </Link>
         </div>
       ))}
